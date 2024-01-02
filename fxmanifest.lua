b9a4a31db50ed44e7a95951c7f783a3a301e16b4
JayOHx <119745695+Jay0Hx@users.noreply.github.com>
2024-01-02 11:17:35 +0000

    Update fxmanifest.lua
    sorted

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,49 +1,8 @@
-local carrying = {}
---carrying[source] = targetSource, source is carrying targetSource
-local carried = {}
---carried[targetSource] = source, targetSource is being carried by source
+fx_version 'cerulean'
 
-RegisterServerEvent("CarryPeople:sync")
-AddEventHandler("CarryPeople:sync", function(targetSrc)
-	local source = source
-	local sourcePed = GetPlayerPed(source)
-   	local sourceCoords = GetEntityCoords(sourcePed)
-	local targetPed = GetPlayerPed(targetSrc)
-        local targetCoords = GetEntityCoords(targetPed)
-	if #(sourceCoords - targetCoords) <= 3.0 then 
-		TriggerClientEvent("CarryPeople:syncTarget", targetSrc, source)
-		carrying[source] = targetSrc
-		carried[targetSrc] = source
-	end
-end)
+game 'gta5'
+author 'Jay0Hx'
+description 'Super simple ragdoll script.'
+version '1.0'
 
-RegisterServerEvent("CarryPeople:stop")
-AddEventHandler("CarryPeople:stop", function(targetSrc)
-	local source = source
-
-	if carrying[source] then
-		TriggerClientEvent("CarryPeople:cl_stop", targetSrc)
-		carrying[source] = nil
-		carried[targetSrc] = nil
-	elseif carried[source] then
-		TriggerClientEvent("CarryPeople:cl_stop", carried[source])			
-		carrying[carried[source]] = nil
-		carried[source] = nil
-	end
-end)
-
-AddEventHandler('playerDropped', function(reason)
-	local source = source
-	
-	if carrying[source] then
-		TriggerClientEvent("CarryPeople:cl_stop", carrying[source])
-		carried[carrying[source]] = nil
-		carrying[source] = nil
-	end
-
-	if carried[source] then
-		TriggerClientEvent("CarryPeople:cl_stop", carried[source])
-		carrying[carried[source]] = nil
-		carried[source] = nil
-	end
-end)
+client_scripts { 'client/*.lua' }
